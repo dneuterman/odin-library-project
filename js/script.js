a103843d8a2dd3714cc3472c27343f7bf04e3c49
Donald Neuterman <dneuterman@gmail.com>
2022-04-30 21:50:32 -0400

    changed Book object to class syntax

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 let userLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
 const addBookToLibrary = (title, author, pages, read) => {
